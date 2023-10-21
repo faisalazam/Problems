2661b6d5e2fa5354be17309b025b66f36870f93e
Crazy <faisalazam_1616@hotmail.com>
2023-10-21 12:30:27 +0500

    Added comments and more solutions.

--- src/com/algorithms/leetcode/medium/ConvertSortedListToBinarySearchTree.java
+++ src/com/algorithms/leetcode/medium/ConvertSortedListToBinarySearchTree.java
@@ -1,14 +1,14 @@
 package com.algorithms.leetcode.medium;
 
+/**
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ * <p>
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the
+ * two subtrees of every node never differ by more than 1.
+ * <p>
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ */
 public class ConvertSortedListToBinarySearchTree {
-    /**
-     * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
-     * <p>
-     * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the
-     * two subtrees of every node never differ by more than 1.
-     * <p>
-     * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
-     */
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums.length == 0) {
             return null;
@@ -20,14 +20,14 @@ public class ConvertSortedListToBinarySearchTree {
         if (left > right) {
             return null;
         }
-        int mid = left + (right - left) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
+        final int mid = left + ((right - left) / 2);
+        final TreeNode root = new TreeNode(nums[mid]);
         root.left = constructBST(nums, left, mid - 1);
         root.right = constructBST(nums, mid + 1, right);
         return root;
     }
 
-    private class TreeNode {
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
