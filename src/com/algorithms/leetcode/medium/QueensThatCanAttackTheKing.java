1e023290c5c5f718064cc27437631b0e5ea41bba
Crazy <faisalazam_1616@hotmail.com>
2023-10-21 13:00:26 +0500

    Added comments and more solutions.

--- src/com/algorithms/leetcode/medium/QueensThatCanAttackTheKing.java
+++ src/com/algorithms/leetcode/medium/QueensThatCanAttackTheKing.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * On a 0-indexed 8 x 8 chessboard, there can be multiple black queens ad one white king.
+ * <p>
+ * You are given a 2D integer array queens where queens[i] = [xQueeni, yQueeni] represents the position of the ith
+ * black queen on the chessboard. You are also given an integer array king of length 2 where king = [xKing, yKing]
+ * represents the position of the white king.
+ * <p>
+ * Return the coordinates of the black queens that can directly attack the king. You may return the answer in any order.
+ * <p>
+ * https://leetcode.com/problems/queens-that-can-attack-the-king/
+ */
 public class QueensThatCanAttackTheKing {
-    /**
-     * On an 8x8 chessboard, there can be multiple Black Queens and one White King.
-     * <p>
-     * Given an array of integer coordinates queens that represents the positions of the Black Queens,
-     * and a pair of coordinates king that represent the position of the White King,
-     * return the coordinates of all the queens (in any order) that can attack the King.
-     * <p>
-     * https://leetcode.com/problems/queens-that-can-attack-the-king/
-     */
     public List<List<Integer>> queensAttackTheKing(int[][] queens, int[] king) {
-        List<List<Integer>> results = new ArrayList<>();
+        final List<List<Integer>> results = new ArrayList<>();
 
-        boolean[][] seen = new boolean[8][8]; //an array to keep track where the queens are seen
+        final int boardSize = 8;
+        final boolean[][] seen = new boolean[boardSize][boardSize]; //an array to keep track where the queens are seen
         for (int[] queen : queens) {
             seen[queen[0]][queen[1]] = true;
         }
 
-        int[] directions = {-1, 0, 1}; // the following loops will traverse through all directions
+        final int[] directions = {-1, 0, 1}; // the following loops will traverse through all directions
         for (int dRow : directions) {
             for (int dColumn : directions) {
                 if (dRow == 0 && dColumn == 0) {
@@ -31,7 +34,7 @@ public class QueensThatCanAttackTheKing {
                 int row = king[0];
                 int column = king[1];
 
-                while (row + dRow >= 0 && row + dRow < 8 && column + dColumn >= 0 && column + dColumn < 8) {
+                while (row + dRow >= 0 && row + dRow < boardSize && column + dColumn >= 0 && column + dColumn < boardSize) {
                     row += dRow;
                     column += dColumn;
                     if (seen[row][column]) {
