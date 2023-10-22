a7d6d42e32bd7417da63dd68dfc1789083f1dfb7
Crazy <faisalazam_1616@hotmail.com>
2023-10-22 11:56:58 +0500

    Added comments and more solutions.

--- src/com/algorithms/leetcode/medium/FindDuplicatesInArray.java
+++ src/com/algorithms/leetcode/medium/FindDuplicatesInArray.java
@@ -3,34 +3,34 @@ package com.algorithms.leetcode.medium;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+ * <p>
+ * Find all the elements that appear twice in this array.
+ * <p>
+ * Could you do it without extra space and in O(n) runtime?
+ * <p>
+ * LeetCode 442. Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/
+ * <p>
+ * APPROACH
+ * <p>
+ * If we use HashSet it will be pretty easy solution. We iterate over the array and put elements one by one in
+ * our HashSet, if element already there, it means that we already have seen this element and we put it in our
+ * output list
+ * <p>
+ * But the problem is asking us to not use extra memory other than for the output list. Given the constraints
+ * (i.e all elements in the range [1, n]) of this problem, we can use nums as a HashSet. We just mark each element
+ * nums[nums[i] - 1] with negative sign, i.e turning it into a negative number. What we are doing is essentially
+ * mapping nums[i] value to a nums[i] - 1 index. -1 is to account for the fact that array is zero-based. We won't
+ * be overflowing our array nums as we are guaranteed the elements will be in [1,n] range.
+ * <p>
+ * Important thing to keep in mind, whenever we read a value from nums we should always take it's absolute value,
+ * as that value might have been marked negative by previous iterations.
+ * <p>
+ * You can also think about this technique as a boolean array, where + of each element means Not seen or False,
+ * and - means Seen or True
+ */
 public class FindDuplicatesInArray {
-    /**
-     * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
-     * <p>
-     * Find all the elements that appear twice in this array.
-     * <p>
-     * Could you do it without extra space and in O(n) runtime?
-     * <p>
-     * LeetCode 442. Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/
-     * <p>
-     * APPROACH
-     * <p>
-     * If we use HashSet it will be pretty easy solution. We iterate over the array and put elements one by one in
-     * our HashSet, if element already there, it means that we already have seen this element and we put it in our
-     * output list
-     * <p>
-     * But the problem is asking us to not use extra memory other than for the output list. Given the constraints
-     * (i.e all elements in the range [1, n]) of this problem, we can use nums as a HashSet. We just mark each element
-     * nums[nums[i] - 1] with negative sign, i.e turning it into a negative number. What we are doing is essentially
-     * mapping nums[i] value to a nums[i] - 1 index. -1 is to account for the fact that array is zero-based. We won't
-     * be overflowing our array nums as we are guaranteed the elements will be in [1,n] range.
-     * <p>
-     * Important thing to keep in mind, whenever we read a value from nums we should always take it's absolute value,
-     * as that value might have been marked negative by previous iterations.
-     * <p>
-     * You can also think about this technique as a boolean array, where + of each element means Not seen or False,
-     * and - means Seen or True
-     */
     public static List<Integer> findDuplicates(int[] nums) {
         final List<Integer> duplicates = new ArrayList<>();
 
@@ -57,4 +57,25 @@ public class FindDuplicatesInArray {
     public static void main(String[] args) {
         System.out.println(findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
     }
+
+    /*
+     * Brute Force:
+     * Compare all the values in the array with each other to check if there are any duplicates.
+     * Time Complexity: O(N^2)
+     * Space Complexity: O(1)
+     */
+
+    /*
+     * Little Improved:
+     * Sort the array and then compare the adjacent values to check if there are any duplicates.
+     * Time Complexity: O(N * log(N))
+     * Space Complexity: O(1)
+     */
+
+    /*
+     * Little Improved:
+     * Use a HashSet to check for duplicates...
+     * Time Complexity: O(N)
+     * Space Complexity: O(N)
+     */
 }
