2926d09a24f992093c217cddfd9f975417baa1b5
Crazy <faisalazam_1616@hotmail.com>
2023-10-30 00:10:49 +0500

    Added comments and more solutions.

--- src/com/algorithms/geeksforgeeks/easy/treetraversals/DetermineIfTwoTreesAreIdentical.java
+++ src/com/algorithms/geeksforgeeks/easy/treetraversals/DetermineIfTwoTreesAreIdentical.java
@@ -3,23 +3,30 @@ package com.algorithms.geeksforgeeks.easy.treetraversals;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Given two binary trees, the task is to find if both of them are identical or not.
+ * <p>
+ * User task:
+ * Since this is a functional problem you don't have to worry about input, you just have to complete the
+ * function isIdentical() that takes two roots as parameters and returns true or false. The printing is done by the
+ * driver code.
+ * <p>
+ * Expected Time Complexity: O(N).
+ * Expected Auxiliary Space: O(Height of the Tree).
+ * <p>
+ * https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+ */
 public class DetermineIfTwoTreesAreIdentical {
-    /**
-     * Given two binary trees, the task is to find if both of them are identical or not.
-     * <p>
-     * User task:
-     * Since this is a functional problem you don't have to worry about input, you just have to complete the
-     * function isIdentical() that takes two roots as parameters and returns true or false. The printing is done by the driver code.
-     * Expected Time Complexity: O(N).
-     * Expected Auxiliary Space: O(Height of the Tree).
-     * <p>
-     * https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
-     */
     private boolean isIdentical(Node root1, Node root2) {
         // return isIdenticalRecursive(root1, root2);
         return isIdenticalIterative(root1, root2);
     }
 
+    /**
+     * Complexity
+     * Time Complexity : O(min(N, M)), Where N and M are the sizes of the trees
+     * Space Complexity : O(log min(N, M)), due to auxiliary stack space used by recursion calls.
+     */
     private boolean isIdenticalRecursive(Node root1, Node root2) {
         if (root1 == null && root2 == null) {
             return true;
@@ -32,15 +39,15 @@ public class DetermineIfTwoTreesAreIdentical {
     }
 
     private boolean isIdenticalIterative(Node root1, Node root2) {
-        Queue<Node> queue1 = new LinkedList<>();
+        final Queue<Node> queue1 = new LinkedList<>();
         queue1.offer(root1);
 
-        Queue<Node> queue2 = new LinkedList<>();
+        final Queue<Node> queue2 = new LinkedList<>();
         queue2.offer(root2);
 
         while (!queue1.isEmpty() && !queue2.isEmpty()) {
-            Node temp1 = queue1.poll();
-            Node temp2 = queue2.poll();
+            final Node temp1 = queue1.poll();
+            final Node temp2 = queue2.poll();
             if (temp1 == null && temp2 == null) {
                 if (queue1.isEmpty() && queue2.isEmpty()) {
                     return true;
@@ -57,7 +64,7 @@ public class DetermineIfTwoTreesAreIdentical {
         return queue1.isEmpty() && queue2.isEmpty();
     }
 
-    private class Node {
+    private static class Node {
         int data;
         Node left;
         Node right;
@@ -67,4 +74,22 @@ public class DetermineIfTwoTreesAreIdentical {
             this.left = this.right = null;
         }
     }
+
+    /**
+     * Brute Force Approach
+     * Intuition
+     * Two binary search trees are identical if they have same inorder, preorder and postorder traversal (all).
+     *
+     * Note : We can't find one traversal, say inorder, and if it is the same for both the trees, can we say the given
+     * trees are identical, because we can have two trees with the same inorder traversal, still they can be non-identical.
+     *
+     * Implementation
+     * Create two vectors to store the traversal for both the trees.
+     * Use them to store the traversal for both the trees and compare them.
+     * If all of the inorder, preorder and postorder are same then return true else return false.
+     *
+     * Complexity
+     * Time Complexity : O(N)
+     * Space Complexity : O(N), since using  ArrayList and call stack
+     */
 }
