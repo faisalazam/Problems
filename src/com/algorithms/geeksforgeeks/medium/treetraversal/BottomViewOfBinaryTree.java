dec7b8f051d33330977bcd1083fff74c36df6ef8
Crazy <faisalazam_1616@hotmail.com>
2023-10-30 11:06:07 +0500

    Added comments and more solutions.

--- src/com/algorithms/geeksforgeeks/medium/treetraversal/BottomViewOfBinaryTree.java
+++ src/com/algorithms/geeksforgeeks/medium/treetraversal/BottomViewOfBinaryTree.java
@@ -11,11 +11,11 @@ import java.util.TreeMap;
  * Given a binary tree, print the bottom view from left to right.
  * A node is included in bottom view if it can be seen when we look at the tree from bottom.
  *                       20
- *                     /    \
- *                   8       22
- *                 /   \        \
- *               5      3       25
  *                     /   \
+ *                   8      22
+ *                 /   \      \
+ *               5      3      25
+ *                    /   \
  *                   10    14
  * For the above tree, the bottom view is 5 10 3 14 25.
  * If there are multiple bottom-most nodes for a horizontal distance from root,
@@ -24,10 +24,10 @@ import java.util.TreeMap;
  *                       20
  *                     /    \
  *                   8       22
- *                 /   \     /   \
- *               5      3 4     25
- *                      /    \
- *                  10       14
+ *                 /   \    /  \
+ *               5      3  4    25
+ *                       /  \
+ *                     10   14
  * For the above tree the output should be 5 10 4 14 25.
  * <p>
  * https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
