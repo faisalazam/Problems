c49766052f4f17d16c3a577ae7176083b2d95818
Crazy <faisalazam_1616@hotmail.com>
2023-10-30 12:00:20 +0500

    Added comments and more solutions.

--- src/com/algorithms/geeksforgeeks/easy/SumOfMiddleElementsOfTwoSortedArrays.java
+++ src/com/algorithms/geeksforgeeks/easy/SumOfMiddleElementsOfTwoSortedArrays.java
@@ -1,5 +1,8 @@
 package com.algorithms.geeksforgeeks.easy;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Given 2 sorted arrays A and B of size N each. Print sum of middle elements of
  * the array obtained after merging the given arrays.
@@ -7,19 +10,22 @@ package com.algorithms.geeksforgeeks.easy;
  * https://practice.geeksforgeeks.org/problems/sum-of-middle-elements-of-two-sorted-arrays/0
  */
 public class SumOfMiddleElementsOfTwoSortedArrays {
-    private static int findMidSum(int[] A, int[] B, int N) {
+
+    // Time Complexity: O(k)
+    // Auxiliary Space: O(1)
+    private static int findMidSum(int[] ar1, int[] ar2, int n) {
         int i = 0;
         int j = 0;
         int k = 0;
-        int K = N;
+        int K = n;
         int K1 = K + 1;
         int kValue = -1;
-        while (i < N && j < N) {
+        while (i < n && j < n) {
             int value;
-            if (A[i] <= B[j]) {
-                value = A[i++];
+            if (ar1[i] <= ar2[j]) {
+                value = ar1[i++];
             } else {
-                value = B[j++];
+                value = ar2[j++];
             }
             if (++k == K) {
                 kValue = value;
@@ -27,21 +33,52 @@ public class SumOfMiddleElementsOfTwoSortedArrays {
                 return kValue + value;
             }
         }
-        while (i < N) {
-            int value = A[i++];
+        while (i < n) {
+            int value = ar1[i++];
             if (++k == K) {
                 kValue = value;
             } else if (k == K1) {
                 return kValue + value;
             }
         }
-        while (j < N) {
-            int value = B[j++];
+        while (j < n) {
+            int value = ar2[j++];
             if (++k == K) {
                 kValue = value;
             } else if (k == K1) {
                 return kValue + value;
             }
+        }
+        return -1;
+    }
+
+    /**
+     * Using Min Heap
+     * <p>
+     * Push the elements of both arrays to a priority queue (min-heap).
+     * Pop-out k-1 elements from the front.
+     * Element at the front of the priority queue is the required answer.
+     * <p>
+     * Time Complexity: O(NlogN)
+     * Auxiliary Space: O(2n) => O(n)
+     */
+    private static int findMidSumV1(int[] ar1, int[] ar2, int n) {
+        final Queue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            priorityQueue.offer(ar1[i]);
+            priorityQueue.offer(ar2[i]);
+        }
+
+        int value = 0;
+        int x = 2 * n;
+        while (!priorityQueue.isEmpty()) {
+            if (x == n + 1) {
+                value = priorityQueue.peek();
+            } else if (x == n) {
+                return value + priorityQueue.peek();
+            }
+            priorityQueue.poll();
+            x--;
         }
         return -1;
     }
